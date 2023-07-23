01fd409c7e530eea523477601cf4ea4e7a9f8da3
Lukas Zumvorde <lukaszumvorde@web.de>
2023-07-23 18:08:02 +0200

    Improved formulation and examples

--- symulator/main.py
+++ symulator/main.py
@@ -23,19 +23,13 @@ class Rule:
         return NotImplemented
 
 
-class Attribute:
-    def __init__(self, name, category, value=0, wounds_s=0, wounds_m=0, wounds_k=0):
+class AttributeCategory:
+    def __init__(self, name, wounds_s=0, wounds_m=0, wounds_k=0):
         self.name = name
-        self.category = category
-        self.value = value
         self.wounds_s = wounds_s
         self.wounds_m = wounds_m
         self.wounds_k = wounds_k
-
-    def __eq__(self, other):
-        if isinstance(other, Attribute):
-            return self.name == other.name
-        return NotImplemented
+        self.attributes = []
 
     def endOfScene(self):
         self.wounds_s = 0
@@ -46,8 +40,20 @@ class Attribute:
     def endOfKampaign(self):
         self.wounds_k = 0
 
+
+class Attribute:
+    def __init__(self, name, category, value=0, wounds_s=0, wounds_m=0, wounds_k=0):
+        self.name = name
+        self.category = category
+        self.value = value
+
+    def __eq__(self, other):
+        if isinstance(other, Attribute):
+            return self.name == other.name
+        return NotImplemented
+
     def effectiveDice(self):
-        return max(0, self.value - self.wounds_s - self.wounds_m - self.wounds_k)
+        return max(0, self.value - self.category.wounds_s - self.category.wounds_m - self.category.wounds_k)
 
 
 class Skill:
@@ -61,8 +67,8 @@ class Skill:
         if isinstance(other, Skill):
             return self.name == other.name
         return NotImplemented
-
 skill_fight = Skill("Fighting", "fighting", level=1, cost=2)
+
 
 class Trait:
     def __init__(self, name, description="", cost=7):
@@ -81,6 +87,7 @@ class Effect:
         self.name = name
         self.description = description
         self.successes = successes
+        self.target = None
 effect_damage_physical = Effect("damage_physical")
 effect_damage_mental = Effect("damage_mental")
 effect_damage_social = Effect("damage_social")
@@ -101,10 +108,10 @@ class Maneuver:
 
 
 class Check:
-    def __init__(self, dice: int, skill: Skill, effects: list = [], difficulty: int =0):
+    def __init__(self, dice: int, skill: Skill, effect: Effect, difficulty: int =0):
         self.dice = dice
         self.skill = skill
-        self.effects = []
+        self.effect = effect
         self.difficulty = difficulty
         self.advantage = 0
         self.successes = None
@@ -128,29 +135,59 @@ class Game:
     def __init__(self, name, rules=[]):
         self.name = name
         self.rules = rules
+        self.players = []
+        self.enemies = []
 
+    def simulateCombatRound(self):
+        player_atks = []
+        player_blks = []
+        enemie_atks = []
+        enemie_blks = []
+        for p in players:
+            checks = p.getCombatChecks()
+            for c in checks:
+                if c.effect.name == "damage_physical":
+                    player_atks.append(c)
+                if c.effect.name == "block":
+                    player_blks.append(c)
+        for p in enemies:
+            checks = p.getCombatChecks()
+            for c in checks:
+                if c.effect.name == "damage_physical":
+                    enemie_atks.append(c)
+                if c.effect.name == "block":
+                    enemie_blks.append(c)
+        
 
 class Player:
-    def __init__(self, attributes=[5,5,5,5,5,5,5,5], atk_strat=None, dmg_strat=None, armor=0):
-        self.str = Attribute("Strength", "Physical", value=attributes[0])
-        self.dex = Attribute("Dexterity", "Physical", value=attributes[1])
-        self.wil = Attribute("Will", "Mental", value=attributes[2])
-        self.int = Attribute("Intelect", "Mental", value=attributes[3])
-        self.cha = Attribute("Charisma", "Social", value=attributes[4])
-        self.emp = Attribute("Empathy", "Social", value=attributes[5])
-        self.gea = Attribute("Gear", "Resources", value=attributes[6])
-        self.fin = Attribute("Finanzes", "Resources", value=attributes[7])
+    def __init__(self, attributes=[5,5,5,5,5,5,5,5], atk_strat=None, armor=0):
+        self.physical  = AttributeCategory("Physical")
+        self.mental    = AttributeCategory("Mental")
+        self.social    = AttributeCategory("Social")
+        self.resources = AttributeCategory("Resources")
+        self.categories = [self.physical, self.mental, self.social, self.resources]
+        self.str = Attribute("Strength",  self.physical, value=attributes[0])
+        self.dex = Attribute("Dexterity", self.physical, value=attributes[1])
+        self.wil = Attribute("Will",      self.mental, value=attributes[2])
+        self.int = Attribute("Intelect",  self.mental, value=attributes[3])
+        self.cha = Attribute("Charisma",  self.social, value=attributes[4])
+        self.emp = Attribute("Empathy",   self.social, value=attributes[5])
+        self.gea = Attribute("Gear",      self.resources, value=attributes[6])
+        self.fin = Attribute("Finanzes",  self.resources, value=attributes[7])
+        self.physical.attributes.append(self.str)
+        self.physical.attributes.append(self.dex)
+        self.mental.attributes.append(self.wil)
+        self.mental.attributes.append(self.int)
+        self.social.attributes.append(self.cha)
+        self.social.attributes.append(self.emp)
+        self.resources.attributes.append(self.gea)
+        self.resources.attributes.append(self.fin)
+        self.attributes = [self.str, self.dex, self.wil, self.int, self.cha, self.emp, self.gea, self.fin]
         self.skills = [skill_fight]
         self.traits = []
         self.atk_strat = atk_strat or ai.atk_strat_equal_bias_atk
-        self.dmg_strat = dmg_strat or ai.dmg_strat_equal_bias_att2_sensible
         self.armor = armor
-
-    def otherAttrInCategory(self, attr):
-        for a in [self.str, self.dex, self.wil, self.int, self.cha, self.emp, self.gea, self.fin]:
-            if a.category == attr.category:
-                return a
-        return None
+        self.states = []
 
     def fightingAttr(self):
         if self.str.value >= self.dex.value:
@@ -171,34 +208,31 @@ class Player:
             if a.effectiveDice() <= 0:
                 return True
         return False
-        # attr1 = self.fightingAttr()
-        # attr2 = self.otherAttrInCategory(attr1)
-        # return attr1.effectiveDice() <= 0 or attr2.effectiveDice() <= 0
 
     def takeDmg(self, n):
-        attr1 = self.fightingAttr()
-        attr2 = self.otherAttrInCategory(attr1)
-        val1 = attr1.effectiveDice()
-        val2 = attr2.effectiveDice()
-        # attr1 = self.attr1 - self.attr1_wound_s - self.attr1_wound_m
-        # attr2 = self.attr2 - self.attr2_wound_s - self.attr2_wound_m
-        dmg1_s, dmg2_s = self.dmg_strat(min(self.armor, n), val1, val2)
-        # dmg1_s = self.dmg_strat(min(self.armor, n), val1, val2)
-        # dmg2_s = max(0, min(self.armor, n) - dmg1_s)
-        dmg1_m, dmg2_m = self.dmg_strat(max(0, n - min(self.armor, n)), val1-dmg1_s, val2-dmg2_s)
-        # dmg1_m = self.dmg_strat(max(0, n - min(self.armor, n)), attr1-dmg1_s, attr2-dmg2_s)
-        # dmg2_m = max(0, n - min(self.armor, n)) - dmg1_m
-        attr1.wounds_s += dmg1_s
-        attr2.wounds_s += dmg2_s
-        attr1.wounds_m += dmg1_m
-        attr2.wounds_m += dmg2_m
-        # self.attr1_wound_s += dmg1_s
-        # self.attr2_wound_s += dmg2_s
-        # self.attr1_wound_m += dmg1_m
-        # self.attr2_wound_m += dmg2_m
+        attr = self.fightingAttr()
+        cat = attr.category
+        val = attr1.effectiveDice()
+        cat.wounds_s += min(self.armor, n)
+        cat.wounds_m += max(0, n - min(self.armor, n))
+
+    def getCombatChecks(self, game):
+        return [ self.offensiveCheck(), self.defensiveCheck(self) ]
 
     def offensiveCheck(self):
         return Check(self.offensiveDice(), self.skills[0], effect=effect_damage_physical)
 
     def defensiveCheck(self):
         return Check(self.defensiveDice(), self.skills[0], effect=effect_block)
+
+    def endOfScene(self):
+        for cat in self.categories:
+            cat.endOfScene()
+
+    def endOfMission(self):
+        for cat in self.categories:
+            cat.endOfMission()
+
+    def endOfKampaign(self):
+        for cat in self.categories:
+            cat.endOfKampaign()
